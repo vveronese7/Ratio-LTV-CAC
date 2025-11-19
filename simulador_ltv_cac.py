b4a634b70a9b52e3baaf16dbeea496422620e87b
vveronese7 <vveronese7@gmail.com>
2025-11-19 13:11:53 -0300

    Include author info and signature image
    Added a separator and author information with an image.

--- simulador_ltv_cac.py
+++ simulador_ltv_cac.py
@@ -167,3 +167,14 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig)
+
+st.markdown("---")  # Linha separadora
+st.markdown("""
+**Vinicius Bartelle Veronese**  
+ESPECIALISTA DE PROGRESSO  
+*Progress Specialist*
+""")
+
+# Se quiser incluir a imagem da assinatura (foto ou logo)
+st.image("assinatura2.png", width=120)  # Coloque o caminho da imagem
+
